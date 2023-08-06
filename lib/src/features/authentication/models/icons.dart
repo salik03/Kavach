0cb2560e0e125375ad7bd5c33572c6029f19ade9
salik03 <uddinsalik03@gmail.com>
2023-08-06 15:18:05 +0530

    Dashboard made

---
+++ lib/src/features/authentication/models/icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyFlutterIcon {
+  MyFlutterIcon._();
+
+  static const _kFontFam = 'MyFlutterIcon';
+  static const String? _kFontPkg = null;
+
+  static const IconData robot = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
+
+
+
