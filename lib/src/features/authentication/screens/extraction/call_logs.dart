00e7a07419056e7f176c386bdb5f7db332deada3
salik03 <uddinsalik03@gmail.com>
2023-08-08 22:20:48 +0530

    Call log bug

--- lib/src/features/authentication/screens/extraction/call_logs.dart
+++ lib/src/features/authentication/screens/extraction/call_logs.dart
@@ -29,14 +29,67 @@ class _PhonelogsScreenState extends State<PhonelogsScreen> {
       body: ListView.builder(
         itemCount: callLogs.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(callLogs[index].name ?? callLogs[index].number!),
-            subtitle: Text(callLogs[index].number!),
-
-
+          return GestureDetector(
+            onTap: () {
+              _showCallLogDetailsDialog(callLogs[index]);
+            },
+            child: ListTile(
+              title: Text(callLogs[index].name ?? callLogs[index].number!),
+              subtitle: Text(callLogs[index].number!),
+            ),
           );
         },
       ),
     );
   }
+
+  void _showCallLogDetailsDialog(CallLogEntry callLog) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Call Log Details"),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Name: ${callLog.name ?? 'Unknown'}"),
+              Text("Number: ${callLog.number}"),
+              // Text("Call Type: ${_getCallTypeString(callLogs as CallType)}"),
+              Text("Call Duration: ${callLog.duration} seconds"),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  String _getCallTypeString(CallType callType) {
+    switch (callType) {
+      case CallType.incoming:
+        return "Incoming";
+      case CallType.outgoing:
+        return "Outgoing";
+      case CallType.missed:
+        return "Missed";
+      case CallType.rejected:
+        return "Rejected";
+      case CallType.blocked:
+        return "Blocked";
+      case CallType.voiceMail:
+        return "Voicemail";
+      default:
+        return "Unknown";
+    }
+  }
 }
+
+//Outcoming: 0, Incoming:1
