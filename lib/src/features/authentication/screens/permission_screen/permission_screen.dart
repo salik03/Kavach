242417e373dc6c38efb541bb7d870a330b7e2ea2
PlasmicZ <aviralkamaljain@gmail.com>
2023-08-09 03:40:55 +0530

    Smol changes

--- lib/src/features/authentication/screens/permission_screen/permission_screen.dart
+++ lib/src/features/authentication/screens/permission_screen/permission_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get/get_utils/get_utils.dart';
-import 'package:kavach_2/src/features/authentication/screens/login_screen/login_screen.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import '../../../../constants/colors.dart';
 import '../../../../constants/image_strings.dart';
+import '../login_screen/login_screen.dart';
 
 class PermissionScreen extends StatefulWidget {
   const PermissionScreen({Key? key}) : super(key: key);
@@ -16,53 +14,7 @@ class PermissionScreen extends StatefulWidget {
 }
 
 class _PermissionScreenState extends State<PermissionScreen> {
-  bool locationPermissionGranted = false;
-  bool notificationPermissionGranted = false;
-  bool callLogPermissionGranted = false;
-  bool messagePermissionGranted = false;
-  bool overlayPermissionGranted = false;
-
-  Future<void> _requestLocationPermission() async {
-    final PermissionStatus status = await Permission.location.request();
-    setState(() {
-      locationPermissionGranted = status.isGranted;
-    });
-  }
-
-  Future<void> _requestMessagePermission() async {
-    final PermissionStatus status = await Permission.sms.request();
-    setState(() {
-      messagePermissionGranted = status.isGranted;
-    });
-  }
-
-  Future<void> _requestCallLogPermission() async {
-    final PermissionStatus status = await Permission.phone.request();
-    setState(() {
-      callLogPermissionGranted = status.isGranted;
-    });
-  }
-
-  Future<void> _requestNotificationPermission() async {
-    final PermissionStatus status = await Permission.notification.request();
-    setState(() {
-      notificationPermissionGranted = status.isGranted;
-    });
-  }
-
-  Future<void> _requestOverlayPermission() async {
-    final PermissionStatus status = await Permission.systemAlertWindow.request();
-    setState(() {
-      overlayPermissionGranted = status.isGranted;
-    });
-  }
-
-  bool get allPermissionsGranted =>
-      locationPermissionGranted &&
-          notificationPermissionGranted &&
-          callLogPermissionGranted &&
-          messagePermissionGranted &&
-          overlayPermissionGranted;
+  bool agreedToTerms = false; // Initially not agreed
 
   static const LinearGradient bgradient = LinearGradient(
     colors: gradientColors,
@@ -79,6 +31,7 @@ class _PermissionScreenState extends State<PermissionScreen> {
         systemNavigationBarIconBrightness: Brightness.dark,
       ),
     );
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
@@ -86,160 +39,85 @@ class _PermissionScreenState extends State<PermissionScreen> {
           decoration: BoxDecoration(
             gradient: bgradient,
           ),
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
+          child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Image.asset(
-                  permission,
+                  permission, // Update with the image asset path
                   width: 196,
                   height: 173.97,
                   fit: BoxFit.contain,
                 ),
                 SizedBox(height: 16),
+
                 Container(
-                  decoration: BoxDecoration(
-                    color: dialogueBoxColor,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        spreadRadius: 2,
-                        blurRadius: 4,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
+                  width: double.infinity,
                   padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Text(
-                        'App_Permissions'.tr,
+                        'Terms and Conditions',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
                         ),
                       ),
                       SizedBox(height: 8),
+
+                      // Content
                       Text(
-                        'We understand the nature and sensitivity of this topic and have taken strong measures to ensure that your data is not compromised.\n'.tr,
+                        'Your privacy is our priority. We collect only essential data, and your personal data are protected!\n\n'
+                            'How we work:\n'
+                            'Nischal is a service of Foxintelligence. For the app to be free, we market data for statistical purposes on e-commerce. We do not transmit any data for advertising purposes, targeting, database, or profiling enrichment.\n\n'
+                            'Your data is protected:\n'
+                            'By using Nischal, you will never be targeted by advertisements, neither on our product nor afterwards.',
                         textAlign: TextAlign.center,
                       ),
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Icon(Icons.location_city),
-                        ),
-                        onTap: () async {
-                          await _requestLocationPermission();
-                        },
-                        title: Text("Device Location"),
-                        subtitle: Text(
-                            "We need this permission to find and connect you with people nearby, providing location-based services."),
-                        trailing: Checkbox(
-                          value: locationPermissionGranted,
-                          onChanged: (value) {
-                            // Handle checkbox state change if needed
-                          },
-                        ),
-                      ),
 
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Icon(Icons.notifications),
-                        ),
-                        onTap: () async {
-                          await _requestNotificationPermission();
-                        },
-                        title: Text("Notifications"),
-                        subtitle: Text(
-                            "We need this permission to send you important notifications and updates about your account and potential matches."),
-                        trailing: Checkbox(
-                          value: notificationPermissionGranted,
-                          onChanged: (value) {
-                            // Handle checkbox state change if needed
-                          },
-                        ),
-                      ),
+                      SizedBox(height: 16),
 
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Icon(Icons.call),
-                        ),
-                        onTap: () async {
-                          await _requestCallLogPermission();
-                        },
-                        title: Text("Calls"),
-                        subtitle: Text(
-                            "We need this permission to enable voice calling with other users, enhancing your communication experience."),
-                        trailing: Checkbox(
-                          value: callLogPermissionGranted,
-                          onChanged: (value) {
-                            // Handle checkbox state change if needed
-                          },
-                        ),
-                      ),
-
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Icon(Icons.message),
-                        ),
-                        onTap: () async {
-                          await _requestMessagePermission();
-                        },
-                        title: Text("Messages"),
-                        subtitle: Text(
-                            "We need this permission to send you important messages and updates about your account and potential connections."),
-                        trailing: Checkbox(
-                          value: messagePermissionGranted,
-                          onChanged: (value) {
-                            // Handle checkbox state change if needed
-                          },
-                        ),
-                      ),
-
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Icon(Icons.warning),
-                        ),
-                        onTap: () async {
-                          await _requestOverlayPermission();
-                        },
-                        title: Text("Alert"),
-                        subtitle: Text(
-                            "We need this permission to show timely alerts and reminders on top of other apps about potential matches."),
-                        trailing: Checkbox(
-                          value: overlayPermissionGranted,
-                          onChanged: (value) {
-                            // Handle checkbox state change if needed
-                          },
-                        ),
-                      ),
-
-                      Container(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: allPermissionsGranted
-                              ? () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => LoginScreen(),
-                              ),
-                            );
-                          }
-                              : null,
-                          child: const Text(
-                            'I Agree',
-                            style: TextStyle(color: Colors.white70),
+                      // Agreement Checkbox
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Checkbox(
+                            value: agreedToTerms,
+                            onChanged: (value) {
+                              setState(() {
+                                agreedToTerms = value!;
+                              });
+                            },
                           ),
-                          style: ElevatedButton.styleFrom(
-                            primary: allPermissionsGranted
-                                ? Color(0xFF1D4D4F)
-                                : Colors.white70,
-                          ),
+                          Text('I agree to the terms and conditions'),
+                        ],
+                      ),
+
+                      SizedBox(height: 16), // Add some spacing for better visual separation
+
+                      // Agree Button
+                      ElevatedButton(
+                        onPressed: agreedToTerms
+                            ? () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginScreen(),
+                            ),
+                          );
+                        }
+                            : null,
+                        child: const Text(
+                          'I Agree',
+                          style: TextStyle(color: Colors.white70),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: agreedToTerms ? Color(0xFF1D4D4F) : Colors.white70,
                         ),
                       ),
                     ],
